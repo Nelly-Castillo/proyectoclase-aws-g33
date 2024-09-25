0bd76e2aa26a9c06f5980bdc928e547fe99561fe
andreaagt <andgt3@gmail.com>
2024-09-25 16:23:46 -0600

    cors added

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
-const port = 3000
+require('dotenv').config()
+const port = process.env.PORT
 
 const ContactsRoutes = require('./src/routes/contactRoutes')
 
-app.use(express.json())
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
+app.use(express.json())
+app.use(cors(corsOptions))
 
 // RUTA DE CONTACTOS
 app.use('/contactos', ContactsRoutes)
-
-
 
 
 app.listen(port, () => {
